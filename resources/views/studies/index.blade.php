92c6b9b83c81fa46c365089d78261ee50b125c11
Wa-www <0nicoco0@gmail.com>
2020-06-23 18:11:35 +0900

    login navbar

--- resources/views/studies/index.blade.php
+++ resources/views/studies/index.blade.php
@@ -25,10 +25,10 @@
             会員メニュー
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="/login">ログイン</a>
+            <a class="dropdown-item" href="/register">新規登録</a>
             <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
+            <a class="dropdown-item" href="logout">ログアウト</a>
           </div>
         </li>
         <li class="nav-item">
